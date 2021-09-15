2e24324fa76fbd6290460007bd65f8f554d550af
Keerthana-A-V <avkeerthanakeerthana78@gmail.com>
2021-09-15 18:49:07 +0530

    storing in cloud

--- js/script.js
+++ js/script.js
@@ -19,41 +19,11 @@ path=new Array();
 
     let trid=document.getElementById("trid").value;
      trlist=trid.split(",");
-    if(trlist.length=="6"){
+    if(trlist.length>="6"){
         merkletreepath();
-     /*   flag=1;
-        if(flag==1){
-    let trid=document.getElementById("trid").value;
-    let trlist=trid.split(",");
-    tree=new MerkleTree(trlist);
-    tree.createTree();
-    rootHash=tree.getRootHash();
-    document.getElementById("waiting").innerHTML=" ";
-    document.getElementById("otp").innerHTML="<br><b style='color:black'>Root : </b>";
-    var id1=document.querySelector("#otp");
-    var newdiv= document.createElement('sm-copy')
-     newdiv.value=rootHash;
-     id1.appendChild(newdiv);
-    }
-    }
-        //path
-    for(let i=0;i<trlist.length;i++){
-        //let path[i] = tree.getHashPathToRoot(trlist[i]);
-        path.push(tree.getHashPathToRoot(trlist[i]));
-        }
-        for(let i=0;i<trlist.length;i++){
 
-                var id1=document.querySelector("#otp");
-                var newdiv1= document.createElement('div')
-                 newdiv1.innerHTML="<br><b style='color:black'>path["+trlist[i]+"] =</b> ";
-                 id1.appendChild(newdiv1);
-
-                var id1=document.querySelector("#otp");
-                var newdiv= document.createElement('sm-copy')
-                 newdiv.value=path[i];
-                 id1.appendChild(newdiv);
-            }*/
      } })
+
 
      function merkletreepath(){
 
@@ -86,20 +56,34 @@ path=new Array();
                  newdiv.value=path[i];
                  id1.appendChild(newdiv);
         }
-    }
+        //writing into cloud
+        options={receiverID:"FKAEdnPfjXLHSYwrXQu377ugN4tXU7VGdf",application:"Tree_root",comment:"Storing roothash in cloud"}
+        floCloudAPI.sendGeneralData(rootHash,"Merkle_Tree",options).then(
+            function(value){
+                console.log(value);
+            },
+            function(error){
+                console.log(error);
+            })
+        }
     //verify path
 
 document.getElementById("vrbtn").addEventListener('click',()=>{
     let roothash=document.getElementById("roothash").value;
+
+  console.log(floCloudAPI.requestGeneralData("Merkle_Tree",{}))
+
+
     let ele=document.getElementById("ele").value;
+    tree=new MerkleTree(trlist);
     path_string=document.getElementById("path11").value;
     path1=path_string.split(',')
     let verification=tree.verifyMerkleMembership(ele,roothash,path1);
     if(verification==true){
-        document.getElementById("otp2").innerHTML="<br><b style='color:black'>TRUE!, '"+ele+"' is PRESENT in</b>' "+roothash+" ' !";
+        document.getElementById("otp2").innerHTML="<span class='otpver'><b style='color:black;'>TRUE!, '"+ele+"' is PRESENT in</b>' "+roothash+" ' !</span>";
     }
     else{
-        document.getElementById("otp2").innerHTML="<br><b style='color:black'>FALSE!, ' "+ele+" ' is NOT PRESENT in</b> ' "+roothash+" ' !";
+        document.getElementById("otp2").innerHTML="<span class='otpver'><b style='color:black;'>FALSE!, ' "+ele+" ' is NOT PRESENT in</b> ' "+roothash+" ' !</span>";
 
     }
 })
